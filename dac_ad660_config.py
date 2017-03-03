c122da478f0341c5eefe91491df4709cda192dd0
jayich <jayich@gmail.com>
2017-03-03 12:40:58 -0800

    added config file

--- dac_ad660_config.py
+++ dac_ad660_config.py
@@ -4,7 +4,7 @@ class channelConfiguration(object):
     """
 
     def __init__(self, dacChannelNumber, trapElectrodeNumber = None,
-                 octant = None, name = None, boardVoltageRange = (-10, 10),
+                 octantNumber = None, name = None, boardVoltageRange = (-10, 10),
                  allowedVoltageRange = (-10, 10)):
         
         self.dacChannelNumber = dacChannelNumber
@@ -19,3 +19,25 @@ class channelConfiguration(object):
 
     def computeDigitalVoltage(self, analogVoltage):
         return int(round(sum([ self.calibration[n] * analogVoltage ** n for n in range(len(self.calibration)) ])))
+
+
+class hardwareConfiguration(object):
+    EXPNAME = 'Qsim'
+    okDeviceID = 'DAC_AD660'
+    okDeviceFile = 'dac.bit'
+    PREC_BITS = 16
+    pulseTriggered = True
+    maxCache = 126
+    filter_RC = 5e4 * 4e-7
+
+    elec_dict = {
+        '0': channelConfiguration(2, trapElectrodeNumber=2),
+        '1': channelConfiguration(4, trapElectrodeNumber=4),
+        '2': channelConfiguration(6, trapElectrodeNumber=6),
+        '3': channelConfiguration(8, trapElectrodeNumber=8),
+        '4': channelConfiguration(10, trapElectrodeNumber=10),
+        '5': channelConfiguration(15, trapElectrodeNumber=15),
+        '6': channelConfiguration(17, trapElectrodeNumber=17),
+        '7': channelConfiguration(19, trapElectrodeNumber=19),
+        #'8': channelConfiguration(21, trapElectrodeNumber=21)
+        }
