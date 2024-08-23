155f850ac643ebcb642dd0af4c5a88e123f08922
thomasdellaert <dellaert.thomas@gmail.com>
2024-08-22 17:01:47 -0700

    minor changes to make the UI look and behave a little nicer

--- switch_client_config.py
+++ switch_client_config.py
@@ -1,12 +1,12 @@
 class switch_config(object):
-    '''
-    configuration file for multiplexer client
+    """
+    configuration file for arduinoTTL client
     info is the configuration dictionary in the form
-    {channel_name: (port, display_location, inverted)), }
-    '''
+    {channel_name: (port, inverted)), }
+    """
     info = {
-            '399 trap': (10, (1, 0), False),
+            '399 Shutter': (10, True),
             # 'State Detection Shutter': (12, (3, 0), False),
-            'Bad WM Switch': (7, (5, 0), False)
+            # 'Bad WM Switch': (7, (5, 0), False)
     }
 
