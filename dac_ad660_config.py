d7036eb25dd064921ed451c3b4f931d643cca129
thomasdellaert <dellaert.thomas@gmail.com>
2024-08-24 18:14:57 -0700

    further refactor of DAC AD660 server and clients

--- dac_ad660_config.py
+++ dac_ad660_config.py
@@ -1,40 +1,59 @@
+from dataclasses import dataclass
+from typing import Tuple
+
 import numpy as np
 
 
 class ChannelConfiguration:
     """
     Stores complete information for each DAC channel
+    'dac_channel_number' is the number of the DAC as defined on the board (as in, what socket it's plugged into)
+    'name' is the name of the DAC as will be displayed in the GUI
+    'allowed_voltage_range' is the allowable range of the DAC channel, expressed as a tuple
+    'displayed' toggles whether the channel is displayed in the GUI
     """
+    def __init__(self, dac_channel_number: int,
+                 name: str = None,
+                 allowed_voltage_range: Tuple[float, float] = (-10, 10),
+                 displayed: bool = True):
 
-    def __init__(self, dac_channel_number,
-                 name=None,
-                 board_voltage_range=(-10, 10),
-                 allowed_voltage_range=(-10, 10)):
-
-        self.dac_channel_number = dac_channel_number
-        self.board_voltage_range = board_voltage_range
+        self.dac_channel_number: int = dac_channel_number
         self.allowed_voltage_range = allowed_voltage_range
-        self.port_name = str(self.dac_channel_number)
-        if len(self.port_name) == 1:
-            self.port_name = "0" + self.port_name
-        if name is None:
-            self.name = "DAC " + str(dac_channel_number).zfill(2)
-        else:
+        self.port_name = str(self.dac_channel_number).zfill(2)
+        if name:
             self.name = name
+        else:
+            self.name = "DAC " + self.port_name
+        self.displayed = displayed
+
+
+@dataclass
+class Multipole:
+    """
+    Stores the information on a multipole. i.e. a control that will adjust the DACs in a certain way
+    'name' is the name of the multipole, as will be displayed in the GUI
+    'effect' is a list of effects of that multipole on the DACs
+        That is, the ith element of effect is the multipole's effect on the ith DAC
+    'displayed' is a flag that toggles whether the multipole is displayed in the gui
+    """
+    name: str
+    effect: list = None
+    displayed: bool = True
 
 
 class HardwareConfiguration:
-    EXPNAME = 'Qsim'
-    okDeviceID = 'DAC_AD660'
-    okDeviceFile = 'dac.bit'
-    PREC_BITS = 16
-    pulseTriggered = True
-    maxCache = 126
-    filter_RC = 5e4 * 4e-7
+    experiment_name = 'Qsim'
+    ok_device_id = 'DAC_AD660'
+    ok_device_file = 'dac.bit'
+    prec_bits = 16
+    pulse_triggered = True
+    max_cache = 126
+    filter_rc = 5e4 * 4e-7
+    board_voltage_range = (-10.0, 10.0)
 
     dac_channels = [
-        ChannelConfiguration(4),
-        ChannelConfiguration(6),
+        ChannelConfiguration(4, displayed=False),
+        ChannelConfiguration(6, displayed=False),
         ChannelConfiguration(8,  name="End Cap 1", allowed_voltage_range=(0, 10)),
         ChannelConfiguration(10, name="End Cap 2", allowed_voltage_range=(0, 10)),
         ChannelConfiguration(2,  name="RF Rod 1"),
@@ -43,15 +62,33 @@ class HardwareConfiguration:
         ChannelConfiguration(21, name="RF Rod 2"),
     ]
 
-    """The columns of M are the calibrations of the multipoles. """
+    @classmethod
+    def dac_by_port(cls, port):
+        """Convenience method for returning the channel with a specific DAC port"""
+        return {chan.dac_channel_number: chan for chan in cls.dac_channels}[port]
 
-    M = np.array([
-        [1,   0,   0,   0,   0,   0,   0,   0],
-        [0,   1,   0,   0,   0,   0,   0,   0],
-        [0,   0,   1,   1,   0,   0,   0,   0],
-        [0,   0,   1,  -1,   0,   0,   0,   0],
-        [0,   0,   0,   0,   1,   0,   0,   1],
-        [0,   0,   0,   0,   0,   1,   1,   0],
-        [0,   0,   0,   0,   0,  -1,   1,   0],
-        [0,   0,   0,   0,  -1,   0,   0,   1],
-    ])
+    @classmethod
+    def dac_by_name(cls, name):
+        """Convenience method for returning the channel with a specific name"""
+        return {chan.name: chan for chan in cls.dac_channels}[name]
+
+
+class MultipoleConfiguration:
+
+    multipoles = [
+        Multipole("", displayed=False),
+        Multipole("", displayed=False),
+        Multipole("EC Voltage",    effect=[0, 0, 1,  1, 0, 0,  0,  0]),
+        Multipole("EC Difference", effect=[0, 0, 1, -1, 0, 0,  0,  0]),
+        Multipole("RF Push",       effect=[0, 0, 0,  0, 1, 0,  0, -1]),
+        Multipole("DC Push",       effect=[0, 0, 0,  0, 0, 1, -1,  0]),
+        Multipole("DC Squeeze",    effect=[0, 0, 0,  0, 0, 1,  1,  0]),
+        Multipole("RF Squeeze",    effect=[0, 0, 0,  0, 1, 0,  0,  1]),
+    ]
+
+    # construct the multipole matrix. By default, the multipoles correspond one-to-one with the DAC channels
+    M = np.identity(len(multipoles))
+    for i, multipole in enumerate(multipoles):
+        if multipole.effect:
+            M[i] = multipole.effect
+    M = M.transpose()
