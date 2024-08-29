f2597033b4bea761343ea9c89ea0ec482d7047b5
thomasdellaert <dellaert.thomas@gmail.com>
2024-08-29 13:43:04 -0700

    comments

--- piezo_client_config.py
+++ piezo_client_config.py
@@ -4,12 +4,14 @@ class piezo_config(object):
     Info is the configuration dictionary in the form:
     {channel_name: (port, display_location, inverted)), }
     """
+    # 2x2 grid arrangement
     # info = {'1': (1, (1, 0), False),
     #         '2': (2, (1, 1), False),
     #         '3': (3, (3, 0), False),
     #         '4': (4, (3, 1), False),
     #         }
 
+    # 1x4 horizontal line arrangement
     info = {'1': (1, (1, 0), False),
             '2': (2, (1, 1), False),
             '3': (3, (1, 2), False),
